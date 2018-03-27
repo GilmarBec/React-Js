c0e3f6a2d22c1771af3d27a31f990c211dfe0703
GilmarBec <trabalhos4.5.1.4@gmail.com>
2018-03-27 10:43:35 -0300

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+class Message extends React.Component {
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Message />,
+  document.getElementById('root')
+);
